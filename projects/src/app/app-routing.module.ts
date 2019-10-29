cb099628b2695a2175a0ee5b114a1456ea7c07bf
Biswas Nandamuri <biswas.bin2@gmail.com>
2019-10-29 13:47:42 +1100

    Projects stage-2

--- projects/src/app/app-routing.module.ts
+++ projects/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "test-item", component: ProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/src/app/app.module.ts
+++ projects/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjectComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
