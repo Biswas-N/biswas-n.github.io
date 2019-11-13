6016a809bdb42fdc893f04700f4ec0b090be2509
Biswas Nandamuri <biswas.bin2@gmail.com>
2019-11-13 17:33:57 +1100

    Commit 13 Nov

--- projects/src/app/app.module.ts
+++ projects/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjectComponent } from './project/project.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsService } from './shared/projects.service';
+
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 @NgModule({
   declarations: [
@@ -16,9 +22,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ProjectsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- projects/src/environments/environment.ts
+++ projects/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBM9AwmVRYTHWUrniRF_DiPV5SvsZdqvqE",
+    authDomain: "projects-biswas-coffee.firebaseapp.com",
+    databaseURL: "https://projects-biswas-coffee.firebaseio.com",
+    projectId: "projects-biswas-coffee",
+    storageBucket: "projects-biswas-coffee.appspot.com",
+    messagingSenderId: "378615996718",
+    appId: "1:378615996718:web:9778b19779c0ec5ca85abf",
+    measurementId: "G-Y2NX5JE04B"
+  }
 };
 
 /*
