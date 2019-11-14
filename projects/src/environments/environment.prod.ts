22025295697056822c22f3d3b4be6b7d68aa5577
Biswas Nandamuri <biswas.bin2@gmail.com>
2019-11-14 14:57:52 +1100

    Done projects except FOOTER

--- projects/src/environments/environment.prod.ts
+++ projects/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBM9AwmVRYTHWUrniRF_DiPV5SvsZdqvqE",
+    authDomain: "projects-biswas-coffee.firebaseapp.com",
+    databaseURL: "https://projects-biswas-coffee.firebaseio.com",
+    projectId: "projects-biswas-coffee",
+    storageBucket: "projects-biswas-coffee.appspot.com",
+    messagingSenderId: "378615996718",
+    appId: "1:378615996718:web:9778b19779c0ec5ca85abf",
+    measurementId: "G-Y2NX5JE04B"
+  }
 };
