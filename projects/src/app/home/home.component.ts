d90d7aa80ea980726feb999dc4a8ffb1538732de
GIS Admin <GISAdmin@brimbank.vic.gov.au>
2020-02-06 15:52:18 +1100

    Fixed projects hamburger and prod

--- projects/src/app/home/home.component.ts
+++ projects/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    var burger = document.querySelector(".burger");
+    var nav = document.querySelector("#navbarMenuHeroC");
 
+    burger.addEventListener("click", function() {
+      burger.classList.toggle("is-active");
+      nav.classList.toggle("is-active");
+    });
+  }
 }
